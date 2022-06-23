12bd3c247574a7783dcedc817cf069c22a4a25a3
Lucas Couri Trevizan <l@lct.cx>
2022-06-23 16:50:34 +0000

    Replace app.py

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ import os
 
 os.system("wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb")
 os.system("sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb")
-os.system("sudo apt update")
 os.system("sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29")
+os.system("sudo apt update")
 os.system("sudo apt-cache policy mysql-server")
 os.system("sudo apt install ssh vsftpd openjdk-8-jdk mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*")
-os.system("adduser socinftp --home /usr/socin/econect/ftp --shell /bin/bash")
-os.system("sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config")
-os.system("sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config")
-os.system("sed -i 's/PermitRootLogin yes /PermitRootLogin yes/g' /etc/ssh/sshd_config")
+os.system("sudo adduser socinftp --home /usr/socin/econect/ftp --shell /bin/bash")
