34f283ecbff721864f697475053874325411ae8e
Lucas Couri Trevizan <l@lct.cx>
2022-06-24 13:41:25 +0000

    Replace app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ passwd = input()
 print('Selecionar MySql 5.7 e Ubuntu Bionic')
 time.sleep(5)
 
-chPasswd= 'echo ',passwd, '| passwd socinftp --stdin'
+chPasswd = 'echo {passwd}| passwd socinftp --stdin'
+str.join(chPasswd2)
 os.system("wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb")
 os.system("sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb")
 os.system("sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29")
@@ -13,4 +14,4 @@ os.system("sudo apt update")
 os.system("sudo apt-cache policy mysql-server")
 os.system("sudo apt install ssh vsftpd openjdk-8-jdk mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7* -y")
 os.system("sudo useradd socinftp --home /usr/socin/econect/ftp --shell /bin/bash")
-os.system(chPasswd)
+os.system(chPasswd2)
