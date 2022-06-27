50ca8d4733ea8d2ffacf9160c448fed67dad2850
Lucas Couri Trevizan <l@lct.cx>
2022-06-27 15:00:35 +0000

    Upload New File

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo adduser socinftp --home /usr/socin/econect/ftp --shell /bin/bash
+sudo apt update
+sudo apt install ssh vsftpd openjdk-8-jdk mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7* -y
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "write_enable=YES" >> /etc/vsftpd.conf
+echo "local_umask=777" >> /etc/vsftpd.conf
