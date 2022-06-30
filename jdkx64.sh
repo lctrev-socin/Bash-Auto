b34326581b7fdf44128153ccea6d5599803e3927
lctrev-socin <106091178+lctrev-socin@users.noreply.github.com>
2022-06-30 10:41:06 -0300

    Update jdkx64.sh

--- jdkx64.sh
+++ jdkx64.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/Software/JDK
 mkdir -p ~/Software/Ant
-wget -O /tmp/JDKx64.tar.gz https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u332-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u332b09.tar.gz
+wget -O /tmp/JDKx64.tar.gz https://cdn.azul.com/zulu/bin/zulu8.62.0.19-ca-jdk8.0.332-linux_x64.tar.gz
 tar -xvf  /tmp/JDKx64.tar.gz -C ~/Software/JDK
 wget -O /tmp/Ant.tar.gz https://dlcdn.apache.org//ant/binaries/apache-ant-1.9.16-bin.tar.gz
 tar -xvf  /tmp/Ant.tar.gz -C ~/Software/Ant
