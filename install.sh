c602606848d93cafe8f1b8f4627a4e41d8bb1930
lctrev-socin <106091178+lctrev-socin@users.noreply.github.com>
2022-07-10 23:05:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo adduser socinftp --home /usr/socin/econect/ftp --shell /bin/bash
+sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install ssh vsftpd openjdk-8-jdk mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7* -y
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
